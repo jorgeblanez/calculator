186b52112ba75046424cf2ca423b34619a382e5a
Jorge <13945741+jorgeblanez@users.noreply.github.com>
2023-04-11 23:31:00 -0300

    Create Calculator
    Okay, I know i should have made more commits. I got carried away.
    
    I created the calculator with all requirements, and two extra - allowing
    floating points, and making it look nice(ish)
    
    I won't enable backspace nor keystrokes, because frankly, i'm not in the
    mood right now. I'm sure i'll need to do it in the future, and I believe
    i'm more than able to do it.

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 function add(a,b){
-    return (a * 10 + b * 10) / 10;
+    let num = (a * 10 + b * 10) / 10;
+    return Math.round((num + Number.EPSILON) * 100) / 100
+
 }
 
 function subtract(a,b){
-    return (a * 10 - b * 10) / 10;
+    let num = (a * 10 - b * 10) / 10;
+    return Math.round((num + Number.EPSILON) * 100) / 100
 }
 
 function multiply(a,b){
-    return a * b;
+    let num = a * b;
+    return Math.round((num + Number.EPSILON) * 100) / 100
 }
 
 function divide(a,b){
-    return a / b;
+    let num = a / b;
+    return Math.round((num + Number.EPSILON) * 100) / 100
 }
 
 function operate(operation){
+
     switch (operation[1]){
         case "+":
             return add(operation[0],operation[2]);
@@ -23,11 +29,91 @@ function operate(operation){
         case "*":
             return multiply(operation[0],operation[2]);
         case "/":
+            if(operation[2] === 0){
+                return screen.textContent = "Very Funny!";
+            }
             return divide(operation[0],operation[2]);
     }
-        
 
 }
 
+function getOperand(){
+    if (operation.length === 1) {
+        operation = [];
+        screen.textContent = "";
+    };
+    if(this.textContent === "." && number.includes(".")){
+        return
+    }
+    number += this.textContent;
+    screen.textContent += this.textContent;
+}
+
+function getOperator(){
+
+    if (operation.length === 1){
+        operation.push(this.textContent);
+        screen.textContent += " " + this.textContent + " ";
+        return;
+    }
+
+    if (!number) return
+
+    operation.push(Number(number));
+    number = "";
+
+    if(operation.length === 3){
+        let n = operate(operation);
+        operation = [];
+        operation.push(n);
+        operation.push(this.textContent);
+        screen.textContent = n + " " + this.textContent + " ";
+        return;
+    }
+
+    operation.push(this.textContent);
+    screen.textContent += " " + this.textContent + " ";
+}
+
+function getEqualSign(){
+    if (!number){
+        return
+    }
+    else if (operation.length === 2){
+        operation.push(Number(number));
+        number = "";
+        let n = operate(operation);
+        operation = [];
+        operation.push(n);
+        screen.textContent = n;
+        return console.log(operation)
+    }
+
+}
+
+let number = "";
 let operation = [];
+
+
+const operands = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equalSign = document.querySelector("#equal");
+const screen = document.querySelector("#screen");
+const clear = document.querySelector("#clear");
+
+for(operand of operands){
+    opd = operand.addEventListener("click", getOperand);
+}
+
+for(operator of operators){
+    opt = operator.addEventListener("click", getOperator)
+}
+
+eqs = equalSign.addEventListener("click",getEqualSign)
+
+cl = clear.addEventListener("click",function(){
+    screen.textContent = "";
+    operation = [];
+    number = "";
+});
 
